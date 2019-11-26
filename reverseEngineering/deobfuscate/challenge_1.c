8306bde8b828d509eb7e3157a541240fdeeeede8
root <root@localhost.localdomain>
2019-11-25 23:09:24 -0500

    cleaned up challenge 1 slightly and had it write to a file for saving

--- reverseEngineering/deobfuscate/challenge_1.c
+++ reverseEngineering/deobfuscate/challenge_1.c
@@ -3,51 +3,85 @@
 #include <string.h>
 #include <time.h>
 
+unsigned char *deobfuscate(unsigned char *obfuscated_string, time_t start_time, int strlen);
+time_t extract_time(void *buffer);
+char *extract_string(void *buffer);
+int get_file_size(FILE *fptr);
+
+int STRING_OFFSET = 4;
+
 int main(){
 	
 	// Open the file and read it in.
 	FILE *fptr;
-
 	fptr = fopen("Challenge_1_obfuscated.bin", "rb");
-	fseek(fptr, 0, SEEK_END);
-	int file_length = ftell(fptr);
-	rewind(fptr);
-	printf("File size:%d\n", file_length);
+	int file_size = get_file_size(fptr);		
 	
 	// save the file into a buffer	
-	char *buffer = (char *)malloc(file_length * sizeof(char));
-	int read = fread(buffer, sizeof(char), file_length, fptr);
-	if (read != file_length){
+	char *file_contents = (char *)malloc(file_size * sizeof(char));
+	int read = fread(file_contents, sizeof(char), file_size, fptr);
+	if (read != file_size){
 		printf("Issue reading full file\n");
 		exit(1);
 	}
 	// done with file. 
 	fclose(fptr);
 
-	// extract the time from the buffer
-	int time_t_size = sizeof(unsigned int);
-	printf("Size of time_t: %d\n", time_t_size);
-	time_t start_time = *((time_t*)(buffer));
-	printf("Time: %u\n", start_time);
+	time_t start_time = extract_time(file_contents);
 
 	// extract the string from the buffer
-	unsigned char *obfuscated_string = buffer + time_t_size;
+	unsigned char *obfuscated_string = file_contents + STRING_OFFSET;
 	
-	int strlen = file_length - time_t_size;
-	printf("String length: %d\n", strlen); 
+	int strlen = file_size - STRING_OFFSET;
 	
 	printf("Obfuscated string:\n");
-	for (int i = 0; i < file_length - time_t_size; i++){
+	for (int i = 0; i < strlen; i++){
 		printf("%x ", obfuscated_string[i]);
 	}
 	printf("\n");
 	
-	unsigned char *deobfuscated_string = malloc(strlen * sizeof(char));
-	memset(deobfuscated_string, '\0', strlen);
+	// get the original string.	
+	unsigned char *deobfuscated_string = deobfuscate(obfuscated_string, start_time, strlen);	
+	for (int i = 0; i < strlen; i++ ){
+		printf("%c ", deobfuscated_string[i]);
+	}
+	printf("\n");
+	
+	FILE *out_file = NULL;
+
+	out_file = fopen("Challenge_1_deobfuscated.bin", "wb");
+	fwrite(deobfuscated_string, sizeof(unsigned char), strlen, out_file);
+	fclose(out_file);
+	
+	free(deobfuscated_string);
+	free(file_contents);
+	return 0;
+}
+
+// Given a buffer containing the file contents pull the time out and return it.
+time_t extract_time(void *buf){
+	// treat the beggining of the buffer as if it is time_t since that is what it holds.
+	time_t start_time = *((time_t*)(buf));
+	printf("Time: %u\n", start_time);
+	return start_time;
+}
+
+// Given a file ptr, find and return the size of the file.
+int get_file_size(FILE *fptr){ 
+	fseek(fptr, 0, SEEK_END);
+	int file_size = ftell(fptr);
+	rewind(fptr);
+	return file_size;
+}
+
+// return the original string from the obfuscated one.
+unsigned char *deobfuscate(unsigned char *obfuscated_string, time_t start_time, int strlen){
+	// create and null out buffer for deobfuscated string	
+	unsigned char *deobfuscated_string = malloc(strlen * sizeof(char) + 1);
+	memset(deobfuscated_string, '\0', strlen + 1);
 	
 	// deobfuscate
 	time_t modified_time = start_time;
-	printf("Starting time: %d\n", modified_time);
 	time_t shifted_time = 0;
 	unsigned char xored = 0;
 	unsigned char subd = 0;
@@ -55,26 +89,16 @@ int main(){
 	for (int i = 0; i < strlen; i++){
 		modified_time = modified_time * 0x7452845;
 		modified_time += 0x2934563;
-		printf("Modified time: %u\n", modified_time);
 		shifted_time = modified_time >> 0x17;
-		printf("Shifted time: %u\n", shifted_time);
+		
 		c = obfuscated_string + i;
-		printf("Char being deobfed: %x\n", *c);
+		
 		subd = *c - (modified_time & 0xFF);
-		printf("subbed: %u\n", /*(subd & 0xFF)*/ subd);
 		
 		xored = subd ^ (shifted_time & 0xFF);
-		printf("Xored: %u\n", xored);
+		
 		deobfuscated_string[i] = xored;
-		printf("\n");
 	}
-	// printf("%s\n", deobfuscated_string);
-	for (int i = 0; i < strlen; i++ ){
-		printf("%c ", deobfuscated_string[i]);
-	}
-	printf("\n");
-	
-	free(deobfuscated_string);
-	free(buffer);
-	return 0;
+
+	return deobfuscated_string;
 }
