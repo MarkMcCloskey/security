5c37cc8b94ccc3330d9d6e1e30ff8ab3f6ebc2ee
root <root@localhost.localdomain>
2019-12-30 19:15:09 -0500

    challenge 2 solved.

--- reverseEngineering/deobfuscate/challenge_2.c
+++ reverseEngineering/deobfuscate/challenge_2.c
@@ -3,23 +3,30 @@
 #include <string.h>
 #include <time.h>
 
+// Ensure debug is off unless compiled with it defined as: gcc -DDEBUG
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
 #define debug(fmt, ...) \
-	do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while(0)
+	do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
 
 unsigned char *deobfuscate(unsigned char *obfuscated_string, time_t start_time, int strlen);
 time_t extract_time(void *buffer);
 char *extract_string(void *buffer);
 int get_file_size(FILE *fptr);
 
-int STRING_OFFSET = 4;
+int STRING_OFFSET = 1;
 
 int main(){
 	debug("%s\n\n", "DEBUG SET");	
+	
 	// Open the file and read it in.
 	FILE *fptr;
 	fptr = fopen("Challenge_2_obfuscated.bin", "rb");
 	int file_size = get_file_size(fptr);		
-	
+	debug("File size: %d\n", file_size);
+
 	// save the file into a buffer	
 	char *file_contents = (char *)malloc(file_size * sizeof(char));
 	int read = fread(file_contents, sizeof(char), file_size, fptr);
@@ -31,26 +38,27 @@ int main(){
 	fclose(fptr);
 
 	time_t start_time = extract_time(file_contents);
-
+	debug("Time: %u\n", start_time);
+	
 	// extract the string from the buffer
 	unsigned char *obfuscated_string = file_contents + STRING_OFFSET;
-	
 	int strlen = file_size - STRING_OFFSET;
-	
+
+	debug("Obfuscated string length: %d\n", strlen);	
 	debug("%s\n", "Obfuscated string: ");
 	for (int i = 0; i < strlen; i++){
 		debug("%x ", obfuscated_string[i]);
 	}
 	debug("%s", "\n");
 	
-	/*	
+		
 	// get the original string.	
 	unsigned char *deobfuscated_string = deobfuscate(obfuscated_string, start_time, strlen);	
 	for (int i = 0; i < strlen; i++ ){
-		printf("%c ", deobfuscated_string[i]);
+		printf("%c", deobfuscated_string[i]);
 	}
 	printf("\n");
-	
+		
 	FILE *out_file = NULL;
 
 	out_file = fopen("Challenge_2_deobfuscated.bin", "wb");
@@ -58,7 +66,6 @@ int main(){
 	fclose(out_file);
 	
 	free(deobfuscated_string);
-	*/
 	free(file_contents);
 	return 0;
 }
@@ -66,8 +73,7 @@ int main(){
 // Given a buffer containing the file contents pull the time out and return it.
 time_t extract_time(void *buf){
 	// treat the beggining of the buffer as if it is time_t since that is what it holds.
-	time_t start_time = *((time_t*)(buf));
-	debug("Time: %u\n", start_time);
+	time_t start_time = *((unsigned char*)(buf));
 	return start_time;
 }
 
@@ -85,24 +91,57 @@ unsigned char *deobfuscate(unsigned char *obfuscated_string, time_t start_time, 
 	unsigned char *deobfuscated_string = malloc(strlen * sizeof(char) + 1);
 	memset(deobfuscated_string, '\0', strlen + 1);
 	
-	// deobfuscate
-	time_t modified_time = start_time;
-	time_t shifted_time = 0;
-	unsigned char xored = 0;
-	unsigned char subd = 0;
-	unsigned char *c  = 0;
+	
+	// time variables
+	unsigned char time_low = start_time & 0xFF;
+	unsigned char new_time_low = 0, time_left = 0, time_right = 0;
+	
+	// character variables
+	unsigned char *obfuscated_char= 0;
+	unsigned char working_char = 0;
+	unsigned char zeroeth_bit = 0, fifth_bit = 0, fourth_bit = 0;
+
 	for (int i = 0; i < strlen; i++){
-		modified_time = modified_time * 0x7452845;
-		modified_time += 0x2934563;
-		shifted_time = modified_time >> 0x17;
+		// get the current character to deobfuscate
+		obfuscated_char = obfuscated_string + i;
+		debug("Deobfuscating char: %x\n", *obfuscated_char);
 		
-		c = obfuscated_string + i;
+		// handle all operations on the time	
+		time_right = time_low >> 7;
+		time_left = time_low << 1;
+		new_time_low = (time_right | time_left) & 0xFF;
 		
-		subd = *c - (modified_time & 0xFF);
+		// get lowest bit in obfuscated character	
+		fourth_bit = (*obfuscated_char & 0x80) >> 7;
+		debug("Fourth bit: %u\n", fourth_bit);
 		
-		xored = subd ^ (shifted_time & 0xFF);
+		// figure out what the correct zeroeth bit should be
+		zeroeth_bit = ((*obfuscated_char & 4) >> 2) ^ fourth_bit;
+		// zeroeth_bit = ((fourth_bit << 2) ^ (*obfuscated_char & 4)) >> 2;
+		debug("Zeroeth bit: %u\n", zeroeth_bit);
+
+		// figure out what the correct fifth bit should be.
+		fifth_bit = ((*obfuscated_char & 8) >> 3) ^ zeroeth_bit;
+		//fifth_bit = ((zeroeth_bit << 3) ^ (*obfuscated_char & 8)) >> 3;
+		debug("Fifth bit: %u\n", fifth_bit);
 		
-		deobfuscated_string[i] = xored;
+		working_char = 0;
+		// working_char |= zeroeth_bit;
+		// working_char |= ((0xF0 & *obfuscated_char) >> 3);
+
+		// working_char |= (fifth_bit << 5);
+		//working_char |= (sixth_bit << 6);
+		//working_char |= (*obfuscated_char & 2) << 6;
+		//working_char |= ((0x03 & *obfuscated_char) << 6);
+		
+		working_char = ((*obfuscated_char & 3) << 6) | (fifth_bit << 5) | ((*obfuscated_char & 0xF0) >>3) | zeroeth_bit;
+		debug("Before time xor working char: %c\n", working_char);
+		working_char = working_char ^ time_low;
+
+		debug("New time: %x\n", new_time_low);
+		debug("Deobfed char: %c\n", working_char);
+		time_low = new_time_low;
+		deobfuscated_string[i] = working_char;
 	}
 
 	return deobfuscated_string;
